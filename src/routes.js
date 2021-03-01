5b7bdce0209635013da7ddc9ee5523d545a6981d
AlmeidaKing <almeida_jn@hotmail.com>
2021-03-01 09:43:22 -0300

    feat: adding styled components lib

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-/**
- * Arquivo para roteamento das paginas do site
- */
-import Navbar from './components/Navbar/Navbar';
-
 import Home from './pages/home';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Navbar />,
         <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
