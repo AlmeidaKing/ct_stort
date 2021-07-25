0c11eb4e18c658e3cb5ef2a3c7bd4d66c8a7ffff
AlmeidaKing <almeida_jn@hotmail.com>
2021-07-25 17:42:07 -0300

    merged master

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,31 +1,29 @@
-import { css } from 'styled-components'
-import bg_header from './bg_header.jpg';
+import { css } from "styled-components";
+import bg_header from "./bg_header.jpg";
 
 export const HeaderStyle = css`
-    background-image: radial-gradient(rgba(0, 0, 0, .1), rgba(0, 0, 0, .4)), url(${bg_header});
-    background-size: cover;
-    background-position: center;
-    width: 100%;
-    height: 100vh;
+  background-image: radial-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.4)),
+    url(${bg_header});
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #FFF;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
 
-    img {
-        position: absolute;
-        top: 8px;
-        right: 20px;
-        filter: drop-shadow(4px 3px #000);
-    }
+  img {
+    position: absolute;
+    top: 8px;
+    right: 20px;
+    filter: drop-shadow(4px 3px #000);
+  }
 
-    h1 {
-        font-size: 36px;
-        font-family: Avenir;
-        color: #435159;
-        margin-bottom: 8px;
-    }
-`
-
-HelloWorld('Jonathan');
+  h1 {
+    font-size: 36px;
+    color: #fff;
+    margin-bottom: 8px;
+  }
+`;
